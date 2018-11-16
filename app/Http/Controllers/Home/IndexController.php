1c08c72a039f0a3afa4c14d729fccaa7a29bff07
灿烂的元灵狐 <hongfei@shareg.cn>
2018-11-17 00:28:14 +0800

    文章倒序

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -12,9 +12,9 @@ class IndexController extends Controller
     public function index(Request $request)
     {
         if($request->has('title')){
-            $articels = Articel::where('title','like','%'.$request->title.'%')->paginate(3);
+            $articels = Articel::where('title','like','%'.$request->title.'%')->order('id','desc')->paginate(3);
         }else{
-            $articels = Articel::paginate(3);
+            $articels = Articel::order('id','desc')->paginate(3);
         }
         return view('Home.index',[
             'articels' =>$articels
